6c18bd832426b818c149b4aa60301095978305df
Dimitri John Ledkov <dimitri.j.ledkov@intel.com>
2015-04-01 10:37:05 +0100

    Correct LICENSE tag.
    Set LICENSE to MIT as per https://github.com/angular/angular.js/blob/master/LICENSE.
    
    The license name is in the SPDX format (https://spdx.org/licenses/),
    OSI approved, and thus Open https://wiki.openstack.org/wiki/Open
    
    Bump build #.
    
    Change-Id: I74558c0cd6a771bb83fefabbc14cd182d4fa0b45

--- xstatic/pkg/angular/__init__.py
+++ xstatic/pkg/angular/__init__.py
@@ -13,7 +13,7 @@ NAME = __name__.split('.')[-1] # package name (e.g. 'foo' or 'foo_bar')
 
 VERSION = '1.3.7' # version of the packaged files, please use the upstream
                   # version number
-BUILD = '0' # our package build number, so we can release new builds
+BUILD = '1' # our package build number, so we can release new builds
              # with fixes for xstatic stuff.
 PACKAGE_VERSION = VERSION + '.' + BUILD # version used for PyPi
 
@@ -31,7 +31,7 @@ MAINTAINER_EMAIL = 'openstack@sheep.art.pl'
 HOMEPAGE = 'http://angularjs.org'
 
 # this refers to all files:
-LICENSE = '(same as %s)' % DISPLAY_NAME
+LICENSE = 'MIT'
 
 from os.path import join, dirname
 BASE_DIR = join(dirname(__file__), 'data')
