8d809659b75b616461877807c937cb5fa2173b31
Richard Jones <r1chardj0n3s@gmail.com>
2016-09-07 13:21:36 +1000

    Update with corrected xstatic-release
    Change-Id: I57b7d6c3d98d41f253b15b9d1030b04f4e9e7734

--- setup.py
+++ setup.py
@@ -1,17 +1,25 @@
 from setuptools import setup, find_packages
+from xstatic.pkg import angular as xs
 
 # The README.txt file should be written in reST so that PyPI can use
 # it to generate your project's PyPI page.
 long_description = open('README.txt').read()
 
 setup(
-    name='XStatic-Angular',
-    description="""Angular 1.5.8 (XStatic packaging standard)""",
+    name=xs.PACKAGE_NAME,
+    version=xs.PACKAGE_VERSION,
+    description=xs.DESCRIPTION,
     long_description=long_description,
-    maintainer="Rob Cresswell",
-    maintainer_email='robert.cresswell@outlook.com',
-    use_scm_version=True,
-    setup_requires=['setuptools_scm', 'wheel'],
+    classifiers=xs.CLASSIFIERS,
+    keywords=xs.KEYWORDS,
+    maintainer=xs.MAINTAINER,
+    maintainer_email=xs.MAINTAINER_EMAIL,
+    license=xs.LICENSE,
+    url=xs.HOMEPAGE,
+    platforms=xs.PLATFORMS,
     packages=find_packages(),
-    include_package_data=True
+    namespace_packages=['xstatic', 'xstatic.pkg'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[],
 )
